e1cb3b43565a974a85476df5a1ea36d65e5298a8
DIMITRY BESSON <DIMITRY.BESSON@cbc.ca>
2019-01-21 18:27:47 -0500

    Implement body parser to enable receiving post requests

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-const router = express.Router()
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('Welcome to the root route'))
 
 app.post('/post', function (req, res) {
   console.log(req.body)
+
+
+  
+
   res.send('POST request to the homepage')
 })
 
