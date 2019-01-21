ca0f06afc6bb39fe1ff2aad6410f65fe653b016b
DIMITRY BESSON <DIMITRY.BESSON@cbc.ca>
2019-01-21 18:22:30 -0500

    Install body parser

--- index.js
+++ index.js
@@ -6,4 +6,9 @@ const router = express.Router()
 
 app.get('/', (req, res) => res.send('Welcome to the root route'))
 
+app.post('/post', function (req, res) {
+  console.log(req.body)
+  res.send('POST request to the homepage')
+})
+
 app.listen(port, () => console.log(`Server is listening on port ${port}`))
