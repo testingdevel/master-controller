04b1effd48bad6bdb9a521fb9a82cd6963d8e796
testingdevel <46799887+testingdevel@users.noreply.github.com>
2019-01-25 19:37:09 -0500

    Update apiUrls.js

--- apiUrls.js
+++ apiUrls.js
@@ -1,6 +1,6 @@
 const apiUrls = {
   nlu: 'http://nlu-env.n68mcgse5g.us-east-2.elasticbeanstalk.com/',
-  apiClient: ''
+  apiClient: 'http://nodeservereb-env.rphre9jgry.us-east-1.elasticbeanstalk.com/'
 }
 
 module.exports = apiUrls
